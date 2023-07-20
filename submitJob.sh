7347225d423ed3eb0a298798d4b7d4e5914bc680
JUREHMAN <100743677+JUREHMAN@users.noreply.github.com>
2023-07-20 10:49:46 +0200

    Update submitJob.sh
    Some additional comments.

--- submitJob.sh
+++ submitJob.sh
@@ -4,3 +4,5 @@ bsub -q l  basf2 script.py Any_Arguments_Required
 #q for queue
 #l for jobs up to 24 hours.
 #s for jobs taking less than 3 hours
+#for killing the submitted job "bkill <job id>"
+# For checking the job status "bjobs"
